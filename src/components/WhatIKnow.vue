95962dcb74733e08f7c8553bc955493b74e78702
Michael Minor <me@pixelbacon.com>
2018-10-10 18:19:38 -0500

    Damn Aspergers, more grammar fixes.

--- src/components/WhatIKnow.vue
+++ src/components/WhatIKnow.vue
@@ -1,9 +1,8 @@
 <template lang="pug">
   section.whatIKnow
     h2 The Inbetween
-    p Beautiful ideas take a certain amount of chaos. That chaos needs to be respected and guidelines setup whenever possible. It requires resonating with the unknown, appreciation for the known, and comfort in acknowledging the blind spots. All of those requires a team.
-    p With a team, a tribe, anything is possible. Technology does not limit humans, it allows us to thrive. It allows us the best ways to support each other, minimize risk, and urges us all to value how individuals think, not just what they know. When it comes to trends, social or technical or otherwise, "what" we know can become obsolete with a single day.
+    p Beautiful ideas take a certain amount of chaos. That chaos needs to be respected and guidelines setup whenever possible. It requires resonating with the unknown, appreciation for the known, and comfort in acknowledging the blind spots. All of those require a team and an open mind.
+    p With a team, a tribe, anything is possible. Technology does not limit humans, it allows us to thrive. It allows us the best ways to support each other, minimize risk, while urging us to value how individuals think over what they know. When it comes to trends, social or technical or otherwise, "what" we know can become obsolete with a single day.
     p This isn't just a job or a career, it is a genuine passion and curiosity that bucks complacency. It's a hell of a ride and worth the extra hours when required.
     p It's a responsibility to new ideas, cultivating their existence, and growing makers and process.
 </template>
-`
