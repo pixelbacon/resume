30d0591045b53f131c76e436a937eab8c67077ac
Michael Minor <me@pixelbacon.com>
2018-10-10 13:29:45 -0500

    ReachOut: Formspree fixes

--- src/components/ReachOut.vue
+++ src/components/ReachOut.vue
@@ -6,13 +6,13 @@
       v-container(fluid).container--noPadding
         v-layout(row wrap grid-list-lg="$vuetify.breakpoint.mdAndUp")
           v-flex(xs12 md4)
-            v-text-field(v-model="name" label="Name")
+            v-text-field(v-model="name" label="Name" name="name")
           v-flex(xs12 md4)
-            v-text-field(v-model="email" label="Email")
+            v-text-field(v-model="email" label="Email" name="_replyto")
           v-flex(xs12 md4)
-            v-text-field(v-model="phoneNumber" label="Phone Number")
+            v-text-field(v-model="phoneNumber" label="Phone Number" name="phoneNumber")
           v-flex(xs12)
-            v-textarea(v-model="comment" label="Comment/Inquiry")
+            v-textarea(v-model="comment" label="Comment/Inquiry" name="comment")
           v-flex(xs12)
             v-btn(type="submit" color="primary").ml-0 Submit
 </template>
