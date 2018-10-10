3c8bf40fafb206979a9cd6e48872a90876b27c60
Michael Minor <me@pixelbacon.com>
2018-10-10 12:01:20 -0500

    Platter: Smaller bottom margin on sections

--- src/components/Platter.vue
+++ src/components/Platter.vue
@@ -3,8 +3,8 @@
     h2 Platter
     p Over the 2 decades of being a photographer, developer, designer, and many inbetween there's been quite a few languages, frameworks, and tools that were used in the process. Some are pretty rusty but hopefully you get the gist; I don't settle on any particular set of tools.
     v-container(fluid).pl-0.pr-0
-      v-layout(wrap)
-        v-flex(xs12 sm4 md3 v-for="(section, key) in skills" :key="key").p4
+      v-layout(wrap justify-center)
+        v-flex(xs12 sm4 md3 v-for="(section, key) in skills" :key="key").p3
           SectionWithListVue(:title="_.startCase(key)" :list="section")
     //- h2 Capabilities & Experience
     //- ul
@@ -32,22 +32,12 @@ export default class Platter extends Vue {
 </script>
 
 <style scoped lang="stylus">
-ul
-  clearfix()
-
-li
-  +above(3)
-    // display inline-block
-    float left
-    width 50%
-
-  +above(5)
-      width 33%
-
-  +above(6)
-      width 25%
+.sectionWithList > section
+  margin-bottom 0
 
   +$print()
-    float left
-    width 33%
+    font-size 0.8em
+
+    > ul > li
+      font-size 0.2em
 </style>
