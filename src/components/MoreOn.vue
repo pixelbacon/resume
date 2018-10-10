3645f47bd93ba900eefa1d6b12c92dc3d710103b
Michael Minor <me@pixelbacon.com>
2018-10-10 12:00:55 -0500

    More on LinkedIn: Proper variable usage

--- src/components/MoreOn.vue
+++ src/components/MoreOn.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div.moreOn
-    h5.screen More on <a href="linkedIn">LinkedIn</a>.
-    h5.print More on <a href="linkedIn">{{linkedInLiteral}}</a>.
+    h5.screen More on <a :href="linkedIn" target="_blank">LinkedIn</a>.
+    h5.print More on <a :href="linkedIn">{{linkedInLiteral}}</a>.
 </template>
 
 <script lang="ts">
@@ -14,3 +14,8 @@ export default class MoreOn extends Vue {
   public linkedInLiteral = data.linkedInLiteral;
 }
 </script>
+
+<style scoped lang="stylus">
+h5
+  font-weight: $theme.font.weight.extraBold !important
+</style>
