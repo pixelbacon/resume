bc8fe21c63b98e53508e1c4e4223a9ff38a7d93a
Michael Minor <me@pixelbacon.com>
2018-10-09 19:23:02 -0500

    Header: Increased size of 'me'

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,7 @@
       v-icon(color="primary" large) mdi-linkedin-box
     v-container()
       v-layout(align-center justify-center row wrap)
-        v-flex(xs10 sm6 md5 lg3).p4
+        v-flex(xs10 sm6 md5).p4
           img(src="../assets/me.jpg").me
         v-flex(xs12)
           h1.name Michael Minor
