f783b429d243cc4e627f20c56e2c6ce1bb858977
Michael Minor <me@pixelbacon.com>
2018-10-22 15:19:11 -0500

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,17 @@
     FilterBar.screen
     v-content
       router-view
+    v-footer(app dark height="auto").footer
+      v-container(fluid)
+        v-layout(align-center justify-space-between)
+          div.item Copyright {{year}}
+          div.item Built w/ Typescript & Vue.
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { namespace } from 'vuex-class';
+import moment from 'moment';
 import DebugVue from '@/components/Debug.vue';
 import FilterBar from '@/components/FilterBar.vue';
 
@@ -27,11 +33,18 @@ export default class Home extends Vue {
   public miniVariant = false;
   public right = true;
   public rightDrawer = false;
+
+  public get year(): string {
+    return moment().format('YYYY');
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-.filterBar
-  position
+.footer
+  font-size 0.8em !important
+
+.container
+  padding 0 1em
 </style>
 
