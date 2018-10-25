b8628aca53b3cd1d1258102cdcd291efe3f00cf1
Michael Minor <me@pixelbacon.com>
2018-10-25 15:11:34 -0500

    NOOP: Commenting out for now, no type

--- src/plugins/devtools.ts
+++ src/plugins/devtools.ts
@@ -1,12 +1,12 @@
-import devtools from '@vue/devtools';
-import localtunnel from 'localtunnel';
+// import devtools from '@vue/devtools';
+// import localtunnel from 'localtunnel';
 
-if (process.env.NODE_ENV === 'development') {
-  const port = 8089;
-  const tunnel = localtunnel(port, (err: any, tunnel: any) => {
-    if (err) {
-      return;
-    }
-    devtools.connect(tunnel.url, port);
-  });
-}
+// if (process.env.NODE_ENV === 'development') {
+//   const port = 8089;
+//   const tunnel = localtunnel(port, (err: any, tunnel: any) => {
+//     if (err) {
+//       return;
+//     }
+//     devtools.connect(tunnel.url, port);
+//   });
+// }
